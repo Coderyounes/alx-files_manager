992b46fc45086374061366ab27d29388f4504cac
Coderyounes <younesanas2021@gmail.com>
2024-06-18 12:42:49 +0100

    task 0: Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,8 +1,34 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
 class RedisClient {
-    constructor() {
+  constructor() {
+    this.client = redis.createClient();
+    this.isConnect = null;
+    this.client.on('error', (err) => {
+      console.log(err);
+      this.isConnect = false;
+    });
 
-    }
+    this.client.on('connect', () => { this.isConnect = true; });
+  }
 
-    isAlive()
-    
-};
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    return promisify(this.client.SETEX).bind(this.client)(key, duration, value);
+  }
+
+  async del(key) {
+    return promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+
+export const redisClient = new RedisClient();
+export default redisClient;
