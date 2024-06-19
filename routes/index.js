a885fd90773919147fa9d6cb4d9107f26dc4807d
Coderyounes <younesanas2021@gmail.com>
2024-06-19 17:33:40 +0100

    boilter plate for task 4

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
+import UserController from '../controllers/UserController';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UserController.getMe);
 
 module.exports = router;
